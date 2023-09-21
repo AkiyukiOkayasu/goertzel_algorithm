f8cd9afab52d52349c995dbef3ecb1382c56769d
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2023-09-21 09:19:40 +0900

    Add benchmark

---
+++ benches/benchmark.rs
@@ -0,0 +1,29 @@
+//! Benchmark for the Goertzel algorithm.
+//!
+//! # Usage
+//!
+//! ```sh
+//! cargo bench
+//! ```
+//!
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use goertzel_algorithm::Goertzel;
+
+fn goertzel_benchmark(c: &mut Criterion) {
+    let mut goertzel = Goertzel::new();
+    const SAMPLE_RATE: u32 = 48_000u32;
+    const TARGET_FREQUENCY: f32 = 750.0f32;
+    const BLOCK_SIZE: u32 = 128u32;
+    goertzel.prepare(SAMPLE_RATE, TARGET_FREQUENCY, BLOCK_SIZE);
+    c.bench_function("goertzel", |b| {
+        b.iter(|| {
+            for _i in 0..BLOCK_SIZE {
+                let _r = goertzel.process_sample(black_box(&0.0f32));
+            }
+        })
+    });
+}
+
+criterion_group!(benches, goertzel_benchmark);
+criterion_main!(benches);
